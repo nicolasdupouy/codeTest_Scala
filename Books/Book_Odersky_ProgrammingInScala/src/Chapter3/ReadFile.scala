7331c3c3bb0657d8ae776d3f4ba4f1fb958104ce
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-02-02 12:44:20 +0100

    [Chapter 3] - Read line from a file - Align lines (imperatively)

--- Books/Book_Odersky_ProgrammingInScala/src/Chapter3/ReadFile.scala
+++ Books/Book_Odersky_ProgrammingInScala/src/Chapter3/ReadFile.scala
@@ -5,9 +5,27 @@ import scala.io.Source
 // Program arguments: Books/Book_Odersky_ProgrammingInScala/src/Chapter3/ReadFile.scala
 object ReadFile extends App {
   if (args.length > 0) {
-    for (line <- Source.fromFile(args(0)).getLines())
-      println(line.length + " " + line)
+
+    val lines = Source.fromFile(args(0)).getLines().toList
+    val maxWidth = maxLength(lines)
+
+    for (line <- lines) {
+      val numSpaces = maxWidth - widthOfLength(line)
+      val padding = " " * numSpaces
+      println(padding + line.length + " | " + line)
+    }
   }
   else
     Console.err.println("Please enter filename")
+
+
+  def widthOfLength(s: String) = s.length.toString.length
+
+  def maxLength(lines: List[String]): Int = {
+    var maxWidth = 0
+    for (line <- lines)
+      maxWidth = maxWidth.max(widthOfLength(line))
+
+    return maxWidth
+  }
 }
