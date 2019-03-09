28bc1870052012c66d331e9b10b4daa897b29cb8
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-03-09 16:14:47 +0100

    [Chapter 6] Add add, lessThan and max methods

--- Books/Book_Odersky_ProgrammingInScala/src/Chapter6/Rational.scala
+++ Books/Book_Odersky_ProgrammingInScala/src/Chapter6/Rational.scala
@@ -2,5 +2,20 @@ package Chapter6
 
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  override def toString: String = n + "/" + d
+  val numerator: Int = n
+  val denominator: Int = d
+
+  override def toString: String = numerator + "/" + denominator
+
+  def add(that: Rational): Rational =
+    new Rational(
+      n * that.denominator + that.numerator * d,
+      d * that.denominator
+    )
+
+  def lessThan(that: Rational): Boolean =
+    this.numerator * that.denominator < that.numerator * this.denominator
+
+  def max(that: Rational): Rational =
+    if (this lessThan that) that else this
 }
--- Books/Book_Odersky_ProgrammingInScala/src/Chapter6/RationalMain.scala
+++ Books/Book_Odersky_ProgrammingInScala/src/Chapter6/RationalMain.scala
@@ -3,5 +3,19 @@ package Chapter6
 object RationalMain extends App {
   val x = new Rational(1 , 3)
   val y = new Rational(5, 7)
-  //val z = new Rational(6, 0)
+
+  val z1 = x.add(y)
+  val z2 = x add y
+
+  println("z1 = " + z1)
+  println("z2 = " + z2)
+
+  val oneHalf = new Rational(1, 2)
+  val twoThirds = new Rational(2, 3)
+
+  val oneHalfPlusTwoThirds = oneHalf add twoThirds
+  println(oneHalfPlusTwoThirds)
+
+  println(s"$oneHalf is less than $twoThirds is ${oneHalf.lessThan(twoThirds)}")
+  println(s"max of $oneHalf and $twoThirds is ${oneHalf.max(twoThirds)}")
 }
