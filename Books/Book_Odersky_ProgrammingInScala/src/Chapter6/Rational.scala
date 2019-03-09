d9e20593203ad58df615e3f0f70e9060f13ba437
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-03-09 16:44:27 +0100

    [Chapter 6] Add other basic operations

--- Books/Book_Odersky_ProgrammingInScala/src/Chapter6/Rational.scala
+++ Books/Book_Odersky_ProgrammingInScala/src/Chapter6/Rational.scala
@@ -14,12 +14,54 @@ class Rational(n: Int, d: Int) {
 
   def +(that: Rational): Rational =
     new Rational(
-      this.numerator * that.denominator + that.numerator * this.denominator,
-      this.denominator * that.denominator
+      numerator * that.denominator + that.numerator * denominator,
+      denominator * that.denominator
+    )
+
+  def +(i: Int): Rational =
+    new Rational(
+      numerator + i * denominator,
+      denominator
+    )
+
+  def -(that: Rational): Rational =
+    new Rational(
+      numerator * that.denominator - that.numerator * denominator,
+      denominator * that.denominator
+    )
+
+  def -(i: Int): Rational =
+    new Rational(
+      numerator - i * denominator,
+      denominator
+    )
+
+  def *(that: Rational): Rational =
+    new Rational(
+      numerator * that.numerator,
+      denominator * that.denominator
+    )
+
+  def *(i: Int): Rational =
+    new Rational(
+      i * numerator,
+      denominator
+    )
+
+  def /(that: Rational): Rational =
+    new Rational(
+      numerator * that.denominator,
+      denominator * that.numerator
+    )
+
+  def /(i: Int): Rational =
+    new Rational(
+      numerator,
+      denominator * i
     )
 
   def <(that: Rational): Boolean =
-    this.numerator * that.denominator < that.numerator * this.denominator
+    numerator * that.denominator < that.numerator * denominator
 
   def max(that: Rational): Rational =
     if (this < that) that else this
