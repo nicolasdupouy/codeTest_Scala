a7da2d294b8f7a75868827fd4f6889a3de0f0ac2
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-05-03 19:01:53 +0200

    [Chapter 9] Define functions with a first-class function and the placeholder syntax

---
+++ Books/Book_Odersky_ProgrammingInScala/src/Chapter9/FileMatcher.scala
@@ -0,0 +1,20 @@
+package Chapter9
+
+object FileMatcher {
+  private def filesHere = new java.io.File(".").listFiles
+
+  def filesEnding(query: String) =
+    filesMatching(query, _.endsWith(_))
+
+  def filesContaining(query: String) =
+    filesMatching(query, _.contains(_))
+
+  def filesRegex(query: String) =
+    filesMatching(query, _.matches(_))
+
+  private def filesMatching(query: String,
+                    matcher: (String, String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName, query))
+      yield file
+  }
+}
