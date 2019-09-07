70386e4d22658dd0aac4605eab56c284a8508bb5
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-09-07 16:09:30 +0200

    [Chapter 15] Add main to see warning more clearly

--- Books/Book_Odersky_ProgrammingInScala/src/main/scala/Chapter15/Expr.scala
+++ Books/Book_Odersky_ProgrammingInScala/src/main/scala/Chapter15/Expr.scala
@@ -7,3 +7,11 @@ case class Var(name: String) extends Expr
 case class Number(name: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+
+  object Expr {
+    // @unchecked is to avoid warning as we know all patterns are not matched in the following pattern matching.
+    def describe(e: Expr): String = (e: @unchecked) match {
+      case Number(_) => "a number"
+      case Var(_) => "a variable"
+    }
+  }
