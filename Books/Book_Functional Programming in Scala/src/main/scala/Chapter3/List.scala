a5da8442ad7075f760b11b289ae321c01ec28a3c
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-12-10 11:56:38 +0100

    [FP in Scala - Chapter 3] Exercice 3.1 and 3.2

--- Books/Book_Functional Programming in Scala/src/main/scala/Chapter3/List.scala
+++ Books/Book_Functional Programming in Scala/src/main/scala/Chapter3/List.scala
@@ -1,10 +1,13 @@
 package Chapter3
 
 sealed trait List[+A]
+
 case object Nil extends List[Nothing]
+
 case class Cons[A](head: A, tail: List[A]) extends List[A]
 
 object List {
+
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
@@ -18,5 +21,10 @@ object List {
     case Nil => 1
     case Cons(0, _) => 0
     case Cons(h, t) => h * product(t)
+  }
+
+  def tail(l: List[Int]): List[Int] = l match {
+    case Nil => throw new MatchError("Empty list has no tail")
+    case Cons(_, t) => t
   }
 }
--- Books/Book_Functional Programming in Scala/src/test/scala/chapter3/ListTest.scala
+++ Books/Book_Functional Programming in Scala/src/test/scala/chapter3/ListTest.scala
@@ -1,17 +1,34 @@
 package chapter3
 
+import Chapter3.{List, Cons, Nil}
 import org.scalatest.{FunSpec, Matchers}
 
 class ListTest extends FunSpec with Matchers {
   describe("Test List") {
     it("sum of List(1, 2, 3, 4) should be 10") {
-      val list123 = Chapter3.List(1, 2, 3, 4)
-      Chapter3.List.sum(list123) should be(10)
+      val list123 = List(1, 2, 3, 4)
+      List.sum(list123) should be(10)
     }
 
     it("product of List(1, 2, 3, 4) should be 24") {
-      val list123 = Chapter3.List(1, 2, 3, 4)
-      Chapter3.List.product(list123) should be(24)
+      val list123 = List(1, 2, 3, 4)
+      List.product(list123) should be(24)
+    }
+  }
+
+  describe("Exercices") {
+    it("Exercice 3.1") {
+      val x = List(1, 2, 3, 4, 5) match {
+        case Cons(x, Cons(2, Cons(4, _))) => x
+        case Nil => 42
+        case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+        case _ => 101
+      }
+      println(s"x = $x")
+    }
+
+    it("Exercice 3.2") {
+      List.tail(List(1, 2, 3, 4, 5)) should be (List(2, 3, 4, 5))
     }
   }
 }
